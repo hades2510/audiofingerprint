3d7acc26646b1d670135a73bfd426e5abf27856e
Radu Dita <hades2510@yahoo.com>
2017-05-12 17:43:33 +0300

    initial commit

--- AudioFingerPrinting/ViewController.swift
+++ AudioFingerPrinting/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var urlField: UITextField!
+    
+    private var mp3_1:Data?
+    private var mp3_2:Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn( _ textField:UITextField ) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
 
+    @IBAction func getTheMp3(_ sender: Any) {
+        guard let url = urlField.text else { return }
+            
+        Alamofire.request( url ).responseJSON{
+            response in
+            
+            guard let json = response.result.value as? [String:Any] else { return }
+            
+            guard let link = json["link"] as? String else { return }
+            
+            Alamofire.request( link ).responseData{
+                response in
+                
+                self.mp3_1 = response.result.value
+            }
+            
+        }
+    }
 
 }
 
